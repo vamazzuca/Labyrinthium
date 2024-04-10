86c46d4666bdf6fabd22acfe5067da053930448a
Alvrrick <35272187+vamazzuca@users.noreply.github.com>
2024-04-09 23:27:46 -0600

    Initialize Project

---
+++ server/Models/room.cs
@@ -0,0 +1,47 @@
+﻿namespace server.Models
+{
+    public class room
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Company { get; set; }
+
+        public int MinParty { get; set; }
+
+        public int MaxParty { get; set; }
+
+        public string Time { get; set; }
+
+        public string ScareLevel { get; set; }
+
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public int AgeRequirement { get; set; }
+
+        public string Website { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Image { get; set; }
+
+        public string AltImage { get; set; }
+
+
+
+
+
+
+
+    }
+}
