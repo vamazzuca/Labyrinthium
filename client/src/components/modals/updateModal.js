deeaeddf40fbe315d45db59b13a6c3501871ce2e
Alvrrick <35272187+vamazzuca@users.noreply.github.com>
2024-06-22 16:56:29 -0600

    Update

--- client/src/components/modals/updateModal.js
+++ client/src/components/modals/updateModal.js
@@ -19,7 +19,7 @@ function UpdateModal() {
     const [imageURL, setImageURL] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [isLoadingImg, setIsLoadingImg] = useState(false);
-    const { userData} = useSelector((state) => state.user)
+    const { updateData} = useSelector((state) => state.user)
 
    
     const uploadImage = async (files) => {
@@ -45,33 +45,34 @@ function UpdateModal() {
         const user = JSON.parse(localStorage.getItem('profile-labyrinthium'));
         if (user) {
             dispatch(getUserUpdate(user?.user?.userName));
+            setUser(user)
         }
     }, [dispatch]);
 
     useEffect(() => {
-        if (userData?.result) {
-            setName(userData.result.name || '');
-            setBio(userData.result.bio || '');
-            setLocation(userData.result.location || '');
-            setImageURL(userData.result.photo || '');
-            setUsername(userData.result.username || '');
+        if (updateData?.result) {
+            setName(updateData.result.name || '');
+            setBio(updateData.result.bio || '');
+            setLocation(updateData.result.location || '');
+            setImageURL(updateData.result.photo || '');
+            setUsername(updateData.result.username || '');
         }
-    }, [userData]);
+    }, [updateData]);
 
-    
+   console.log(updateData)
     const onSubmit = useCallback(async (e) => {
         try {
             setIsLoading(true);
             e.preventDefault();
-            const formData = { id: userData.result.id, username: username, name: name, email: userData.result.email, photo: imageURL, bio: bio, location: location };
+            const formData = { id: updateData.result.id, username: username, name: name, email: updateData.result.email, photo: imageURL, bio: bio, location: location };
             dispatch(updateUser(formData, updateModal));
-           // localStorage.setItem('profile-labyrinthium', JSON.stringify({user: {name: name, email: userData.result.email, userName: username}, token: user.token}))
+            localStorage.setItem('profile-labyrinthium', JSON.stringify({user: {name: name, email: updateData.result.email, userName: username}, token: user.token}))
         } catch (error){
             console.log(error);
         } finally {
             setIsLoading(false);
         }
-    }, [updateModal,  user, username, name, dispatch, bio, location, imageURL, userData]);
+    }, [updateModal,  user, username, name, dispatch, bio, location, imageURL, updateData]);
 
     const bodyContent = (
         <div className="flex h-auto flex-col items-center gap-4">
@@ -98,7 +99,7 @@ function UpdateModal() {
              <Input
                 placeholder="Name"
                 onChange={(e) => setName(e.target.value)}  
-                defaultValue={userData?.result?.name}
+                defaultValue={updateData?.result?.name}
                 maxLength={30}
                 required={true}
                 disabled={isLoading}
@@ -107,7 +108,7 @@ function UpdateModal() {
                 <Input
                     placeholder="Username"
                     onChange={(e) => setUsername(e.target.value)}
-                    defaultValue={userData?.result?.username}
+                    defaultValue={updateData?.result?.username}
                     required={true}
                     maxLength={30}
                     disabled={isLoading}
@@ -116,7 +117,7 @@ function UpdateModal() {
                 <Input
                     placeholder="Location"
                     onChange={(e) => setLocation(e.target.value)}
-                    defaultValue={userData?.result?.location}
+                    defaultValue={updateData?.result?.location}
                     maxLength={250}
                     disabled={isLoading}
                 />
@@ -124,7 +125,7 @@ function UpdateModal() {
                 <Input
                     placeholder="Biography"
                     onChange={(e) => setBio(e.target.value)}
-                    defaultValue={userData?.result?.bio}
+                    defaultValue={updateData?.result?.bio}
                     maxLength={250}
                     disabled={isLoading}
                 />
@@ -143,7 +144,25 @@ function UpdateModal() {
                                 Update
                     </button>
                 </div>
+
+
             </form>
+
+            <div className='flex flex-col w-full gap-2 pb-10'>
+                            <button className='
+                            w-full
+                            font-semibold
+                            rounded-full
+                            text-xl
+                            px-4
+                            py-2
+                            text-[#0B0C10]
+                            transition
+                            hover:opacity-80
+                            bg-red-600 '>
+                                Log Out
+                    </button>
+                </div>
         </div>
         
     )
