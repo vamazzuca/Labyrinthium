417257d95217ab59ba4b58b46aae39394e0ddbbd
Alvrrick <35272187+vamazzuca@users.noreply.github.com>
2024-07-05 12:41:07 -0600

    Update home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -26,7 +26,7 @@ function Home({isLoaded}) {
             <div className="w-full h-full flex flex-col z-10 items-center">
         
                 <div className="w-full h-screen flex flex-col items-center justify-center py-2">
-                    <div className="flex flex-col items-center pb-[18rem] text-center gap-6 px-4">
+                    <div className="flex flex-col items-center pb-[16rem] pt-[2rem] text-center gap-6 px-4">
                         <h1 className="text-white font-semibold text-4xl md:text-6xl text-center max-w-[1000px]">Find your sense of adventure with <span className="text-purple-700">escape rooms</span> across Canada</h1>
                         <p className="text-gray-400 text-md md:text-xl font-medium">Search over 1000+ Canadian escape rooms</p>
                        
